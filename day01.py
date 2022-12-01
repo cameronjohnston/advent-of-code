19dc2e1680d409485ae0a52814586ebf00718de0
CodeSmartConsultants <CodeSmartConsultants@gmail.com>
2022-11-30 21:48:28 -0800

    solution for part 2

--- day01.py
+++ day01.py
@@ -1,4 +1,4 @@
-max = 0
+max = [0, 0, 0]
 
 file1 = open('day01_input01.txt', 'r')
 Lines = file1.readlines()
@@ -8,12 +8,17 @@ cur_elf_total = 0
 for l in Lines:
     val = l.strip()
     if val == '':
-        if cur_elf_total > max:
-            max = cur_elf_total
+        for i in (0, len(max)-1):
+            if cur_elf_total > max[i]:
+                max.insert(i, cur_elf_total)
+                max = max[0:3] # truncate to 3 elements
+                max.sort(reverse=True) # descending
+                break # avoid dupes
         cur_elf_total = 0
     else:
         cur_elf_total += int(val)
 
-print(max)
+# print(max)
+print(sum(max))
 
 
